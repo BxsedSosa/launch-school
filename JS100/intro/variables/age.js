203d6b4cb7dcf2913ac66c0e7795308d1ada7483
BxsedSosa <donovanxabad@gmail.com>
2024-01-13 23:43:30 -0800

    completed a couple of chapter and exercises

--- JS100/intro/variables/age.js
+++ JS100/intro/variables/age.js
@@ -1,7 +1,7 @@
-const AGE = 20
+let rlSync = require('readline-sync')
 
-console.log(`You are ${AGE} years old`)
-console.log(`In 10 years, you will be ${AGE + 10} years old`)
-console.log(`In 20 years, you will be ${AGE + 20} years old`)
-console.log(`In 30 years, you will be ${AGE + 30} years old`)
-console.log(`In 40 years, you will be ${AGE + 40} years old`)
+let age = Number(rlSync.question(`How old are you?\n`))
+console.log(`In 10 years, you will be ${age + 10} years old`)
+console.log(`In 20 years, you will be ${age + 20} years old`)
+console.log(`In 30 years, you will be ${age + 30} years old`)
+console.log(`In 40 years, you will be ${age + 40} years old`)
