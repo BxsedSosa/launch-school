99e26f3d6699872341330cb4d9c1f3017a0da2ba
BxsedSosa <donovanxabad@gmail.com>
2024-01-20 20:50:21 -0800

    Completed refactoring the calculator

--- JS101/small-programs/calculatorRefactored.js
+++ JS101/small-programs/calculatorRefactored.js
@@ -6,28 +6,57 @@
 
 const readline = require('readline-sync');
 
-console.log('Welcome to Calculator! \n');
-
-console.log("What's the first number?");
-let number1 = readline.question();
-
-console.log("What's the second number?");
-let number2 = readline.question();
-
-console.log('What Operator would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
-let operator = readline.question();
-
-let output;
-if(operator === '1') {
-  output = Number(number1) + Number(number2);
-} else if (operator === '2') {
-  output = Number(number1) - Number(number2);
-} else if (operator === '3') {
-  output = Number(number1) * Number(number2);
-} else if (operator === '4') {
-  output = Number(number1) / Number(number2);
-} else {
-  console.log('Enter valid operator!')
+function prompt(message) {
+  console.log(`=> ${message}`);
 }
 
-console.log(`The result is ${output}`);
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+prompt('Welcome to Calculator! \n');
+
+prompt("What's the first number?");
+let number1 = readline.question();
+
+while (invalidNumber(number1)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number1 = readline.question;
+}
+
+prompt("What's the second number?");
+let number2 = readline.question();
+
+while (invalidNumber(number2)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  number2 = readline.question;
+}
+
+prompt('What Operator would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+let operator = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operator)) {
+  prompt("Must Choose 1, 2, 3 or 4");
+  operator = readline.question;
+}
+
+let output;
+
+switch (operator) {
+  case '1':
+    output = Number(number1) + Number(number2);
+    break;
+  case '2':
+    output = Number(number1) - Number(number2);
+    break;
+  case '3':
+    output = Number(number1) * Number(number2);
+    break;
+  case '4':
+    output = Number(number1) / Number(number2);
+    break;
+  default:
+    console.log('Invalid operator!');
+}
+
+prompt(`The result is ${output}`);
