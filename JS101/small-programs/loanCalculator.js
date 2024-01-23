ff5f5c57d2f23d0927cbc59f457d7bc0c2f95080
BxsedSosa <donovanxabad@gmail.com>
2024-01-23 08:22:05 -0800

    I re-did some code so it covers some edge cases

--- JS101/small-programs/loanCalculator.js
+++ JS101/small-programs/loanCalculator.js
@@ -11,17 +11,42 @@ let readline = require('readline-sync');
 
 
 function loanCalculator() {
-  let loanAmount = readline.question('=> What is your loan amount?: \n');
-  let loanApr = readline.question('=> What is your loan APR?: \n');
-  let loanDuration = readline.question('=> What is the loan duration? (in years): \n');
+  console.log('')
+  prompt('Welcome to Loan Calculator!');
+  console.log('--------------------------------------------------------- \n');
+
+  prompt('What is your loan amount?: \n');
+  let loanAmount = readline.question();
+
+  while (!Number(loanAmount)) {
+    prompt('Enter a number value!: \n');
+    loanAmount = readline.question();
+  }
+
+  prompt('What is your loan APR?: ');
+  prompt('(Enter whole number) \n');
+  let loanApr = parseInt(readline.question());
+
+  while (!Number(loanApr)) {
+    prompt('Enter a number value!: \n');
+    loanApr = parseInt(readline.question());
+  }
+
+  prompt('What is the loan duration?: ');
+  prompt('(Enter duration in years) \n');
+  let loanDuration = readline.question();
 
   let monthlyApr = (loanApr * 0.01) / 12;
   monthlyApr.toFixed(4);
-  let monthlyDuration = loanDuration * 12
+  let monthlyDuration = loanDuration * 12;
   
   let monthlyPayment =  loanAmount * (monthlyApr / (1 - Math.pow((1 + monthlyApr), (-monthlyDuration))));
 
-  console.log(`Your Monthly payments will be $${monthlyPayment.toFixed(2)}`)
+  console.log(`Your Monthly payments will be $${monthlyPayment.toFixed(2)}`);
+}
+
+function prompt(message) {
+  console.log(`=> ${message}`)
 }
 
 loanCalculator()
