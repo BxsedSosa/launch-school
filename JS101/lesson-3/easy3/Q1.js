b993e66ec145d6699cd47ad3ac447bc484cf7cf8
BxsedSosa <donovanxabad@gmail.com>
2024-01-31 07:29:42 -0800

    Completed Q1.js

--- JS101/lesson-3/easy3/Q1.js
+++ JS101/lesson-3/easy3/Q1.js
@@ -1,3 +1,17 @@
 //Write three different ways to remove all of the elements from the following array:
 
 //let numbers = [1, 2, 3, 4];
+
+/*
+  1)
+    numbers = [];
+
+  2)
+    numbers.splice(0, numbers.length);
+
+  3)
+    while (numbers.length) {
+      numbers.pop();
+    }
+*/
+
