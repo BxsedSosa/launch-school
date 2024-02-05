e612453232ef1e0f2e41c099cc044e6e8293ddb3
BxsedSosa <donovanxabad@gmail.com>
2024-02-05 11:20:17 -0800

    Completed Q2.js

--- JS101/lesson-3/medium1/Q2.js
+++ JS101/lesson-3/medium1/Q2.js
@@ -5,3 +5,62 @@
 // Return a new string that swaps the case of all of the letters:
 
 // `tHE mUNSTERS ARE CREEPY AND SPOOKY.`;
+
+/*
+P:
+
+Problem is asking to take the string given, and flip the letters that are uppercase to lower and vice versa for the lowercase
+*/
+
+/*
+E:
+
+Input:
+  "The Munsters are creepy and spooky."
+
+Output:
+  `tHE mUNSTERS ARE CREEPY AND SPOOKY.`
+
+*/
+
+/*
+D:
+
+Data needed:
+  String
+  array
+
+*/
+
+/*
+A:
+  create array called stringArray
+
+  split string into an array with 'split'
+
+  Create a for loop that iterates over the array length
+    -Create a if statement that checks if the index its on is a upper case letter
+      When True then turn the letter to a lowercase 
+
+    -Create a else if statement that checks if the index its on is a lower case letter
+      When True then turn the letter to a uppercase 
+
+*/
+
+//C:
+function reverseCapitazlize(string) {
+  let stringArray = string.split('');
+  
+  for (let i = 0; i < stringArray.length; i++) {
+    if (stringArray[i] === stringArray[i].toUpperCase()) {
+      stringArray[i] = stringArray[i].toLowerCase();
+    } else if (stringArray[i] === stringArray[i].toLowerCase()) {
+      stringArray[i] = stringArray[i].toUpperCase();
+    }
+  }
+  return stringArray.join(''); 
+}
+
+let result = reverseCapitazlize("The Munsters are creepy and spooky.");
+
+console.log(result);
