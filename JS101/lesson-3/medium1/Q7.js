b0ce728bf13a3d2906b702d6684f5b38b4523d77
BxsedSosa <donovanxabad@gmail.com>
2024-02-06 07:08:03 -0800

    Completed Q7.js

--- JS101/lesson-3/medium1/Q7.js
+++ JS101/lesson-3/medium1/Q7.js
@@ -10,3 +10,36 @@
 
 // console.log(answer - 8);
 
+/*
+P:
+
+The question is asking what will output for the following code
+*/
+
+/*
+E:
+
+Input:
+  let answer = 42;
+  let newAnswer = messWithIt(answer);
+  console.log(answer - 8);
+*/
+
+/*
+D:
+
+Data needed:
+  Numbers
+*/
+
+/*
+A:
+
+From my understanding of the code is that though the variable 42 is uesd in line 7. In line 9 we are console logging (answer - 8) which will still use the value 42 because variables are immutable
+
+the output will be:
+
+34
+*/
+
+// Answer: 34
