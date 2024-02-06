b7ab42b86ab779046df1f888e4b2f857ca2a1f70
BxsedSosa <donovanxabad@gmail.com>
2024-02-06 07:03:52 -0800

    Completed Q6.js

--- JS101/lesson-3/medium1/Q6.js
+++ JS101/lesson-3/medium1/Q6.js
@@ -7,3 +7,42 @@
 // Bonus:
 
 // How can you reliably test if a value is NaN?
+
+/*
+P:
+
+problem is asking what the following code will ouput below and bonus on how can you test reliably for the value of NaN
+*/
+
+/*
+E:
+
+Input:
+let nanArray = [NaN];
+
+console.log(nanArray[0] === NaN);
+*/
+
+/*
+D:
+
+Data needed:
+  Array
+
+*/
+
+/*
+A:
+
+I believe that the output of the question will be:
+
+False
+
+Bonus:
+
+to check if the nanArray variable is equal to NaN
+
+You would use:
+
+console.log(isNan(nanArray[0]))
+*/
