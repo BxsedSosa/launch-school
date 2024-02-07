0f4985d9f4060ab821071ab93770f5cba8cfd600
BxsedSosa <donovanxabad@gmail.com>
2024-02-07 07:47:38 -0800

    Completed Q10.js

--- JS101/lesson-3/medium1/Q10.js
+++ JS101/lesson-3/medium1/Q10.js
@@ -11,3 +11,39 @@
 // What will the following function invocation return?
 
 // bar(foo());
+
+/*
+P:
+
+Question is asking what will the function return
+*/
+
+/*
+E:
+
+Input:
+
+bar(foo());
+*/
+
+/*
+D:
+
+data needed:
+  functions
+*/
+
+/*
+A:
+
+I think the function will return 'no'
+
+because foo will return 'yes'and then will be input into the bar parameter then put through the tenary which is not equal to 'no' which returns 'no'
+*/
+
+
+/*
+"no";
+
+This is because the value returned from the foo function will always be "yes" , and "yes" === "no" will be false.
+*/
