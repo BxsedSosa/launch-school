37fce1bb34b17c8f761bf3181d7d31a36673ee0c
BxsedSosa <donovanxabad@gmail.com>
2024-02-08 03:23:19 -0800

    Completed Q3.js

--- JS101/lesson-3/hard1/Q3.js
+++ JS101/lesson-3/hard1/Q3.js
@@ -79,6 +79,19 @@ D:
 /*
 A:
 
-*/
+  A)
+    'one is: two'
+    'two is: three'
+    'three is: two'
 
-//C:
+  B)
+    'one is one'
+    'two is two'
+    'three is three'
+
+  C)
+    'one is: two'
+    'two is: three'
+    'three is: one'
+  
+*/
