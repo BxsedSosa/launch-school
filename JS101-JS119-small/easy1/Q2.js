a7480983655e67d4b2a303f24feb1db3c6e248f3
BxsedSosa <donovanxabad@gmail.com>
2024-02-09 07:35:18 -0800

    Completed Q2.js, Added all questions in

---
+++ JS101-JS119-small/easy1/Q2.js
@@ -0,0 +1,48 @@
+/*
+
+Odd Numbers
+
+Log all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+
+*/
+
+/*
+P:
+
+Question is asking to log all odd numbers from 1 - 99
+*/
+
+/*
+E:
+
+  input:
+    100
+*/
+
+/*
+D:
+
+  data needed:
+    number
+*/
+
+/*
+A:
+
+  create a function called logOdd
+    create a for loop if index < number given increment index
+      if index modulo of 2 equals to 1
+        console.log number 
+*/
+
+//C:
+
+function logOdd(number) {
+  for (let i = 1; i < number; i++) {
+    if (i % 2 === 1) {
+      console.log(i);
+    }
+  }
+}
+
+logOdd(100);
