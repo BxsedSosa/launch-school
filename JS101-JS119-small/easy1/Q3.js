a5a9be79713deae4fe3d096c68ff956b8f31b4b0
BxsedSosa <donovanxabad@gmail.com>
2024-02-09 07:45:19 -0800

    Completed Q3.js

--- JS101-JS119-small/easy1/Q3.js
+++ JS101-JS119-small/easy1/Q3.js
@@ -9,21 +9,40 @@ Log all even numbers from 1 to 99, inclusive, to the console, with each number o
 /*
 P:
 
+  Problem is asking to console log all even numbers between 1 - 99
 */
 
 /*
 E:
 
+  Input:
+    99
 */
 
 /*
 D:
 
+  Data needed:
+    Number
 */
 
 /*
 A:
 
+  create a function called logOdd
+    create a for loop if index < number given increment index
+      if index modulo of 2 equals to 0
+        console.log number 
 */
 
 //C:
+
+function logOdd(number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 2 === 0) {
+      console.log(i);
+    }
+  }
+}
+
+logOdd(99);
