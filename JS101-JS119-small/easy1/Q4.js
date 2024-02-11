5c629ea690109dbc59b2d68429712b9066609515
BxsedSosa <donovanxabad@gmail.com>
2024-02-11 11:57:16 -0800

    Completed Q4.js

--- JS101-JS119-small/easy1/Q4.js
+++ JS101-JS119-small/easy1/Q4.js
@@ -21,21 +21,61 @@ The area of the room is 70.00 square meters (753.47 square feet).
 /*
 P:
 
+Question is asking to create a program that ask for the length and the width of a room then returns square meters and sqft
 */
 
 /*
 E:
 
+  input:
+    user input of lenth
+    user input of width
+
+  output:
+    sq meters and in parentheses sqft
 */
 
 /*
 D:
 
+  data needed:
+    Numbers
 */
 
 /*
 A:
 
+  Create a const readline
+    equal to require('readline-sync')
+
+  Create function called sqMeters
+
+  create a variable called userLength
+    which will have a method of readline.questions()
+      inside the parentheses will have 'Enter the length of the room: \n'
+
+  create a variable called userWidth
+    which will have a method of readline.questions()
+      inside the parentheses will have 'Enter the width of the room: \n'
+
+  create variable result
+    which is going to multiple userLength * userWidth
+
+  then console.log 
+    `The area of the room is ${result} square meters (${result * 10.7639} square feet)
+  
 */
 
 //C:
+
+const readline = require('readline-sync');
+
+function sqMeters() {
+  const sqMeters_To_Sqfeet = 10.7639
+  let userLength = Number(readline.question('Enter the length of the room: \n'));
+  let userWidth = Number(readline.question('Enter the width of the room: \n'));
+  let result = userLength * userWidth;
+  console.log(`The area of the room is ${result} square meters (${result * sqMeters_To_Sqfeet} sqaure feet)`);
+}
+
+sqMeters();
