2fa56e89e87d095d6e2408c752e1b6d9e22d7c87
BxsedSosa <donovanxabad@gmail.com>
2024-02-12 10:53:04 -0800

    Started on Q10.js

--- JS101-JS119-small/easy1/Q10.js
+++ JS101-JS119-small/easy1/Q10.js
@@ -18,21 +18,58 @@ multisum(1000);    // 234168
 /*
 P:
 
+  question is asking number given add all the numbers together that are multiples of 3 or 5
 */
 
 /*
 E:
 
+  Input:             Output:
+  multisum(3);       // 3
+  multisum(5);       // 8
+  multisum(10);      // 33
+  multisum(1000);    // 234168
 */
 
 /*
 D:
+
+  Data needed:
+    numbers
 
 */
 
 /*
 A:
 
+  create function called multisum
+    1 parameter called number
+
+  create a variable total equal to 0
+  
+  create a for loop which iterates over the number
+    if number % 3 equal to 0 or number % 5 equa to 0
+      then add the index to total
+
+    return total
+
 */
 
 //C:
+
+function multisum(number) {
+  let total = 0;
+
+  for (let i = 1; i < number; i++) {
+    if (number % 3 === 0 || number % 5 === 0) {
+      total += i;
+      console.log(i);
+    }
+  }
+  return total;
+}
+debugger;
+console.log(multisum(3));       // 3
+console.log(multisum(5));       // 8
+console.log(multisum(10));      // 33
+console.log(multisum(1000));    // 234168
--- JS101-JS119-small/easy1/Q9.js
+++ JS101-JS119-small/easy1/Q9.js
@@ -29,21 +29,84 @@ isLeapYear(400);       // true
 /*
 P:
 
+  Question is asking to take the previous question but add a new stipulation to it. IF the year is before 1752 then any number divisible by 4 is a considered a leap year. After 1752 will still be the same question as before
 */
 
 /*
 E:
+
+  Input:                 Output:
+  isLeapYear(2016);      // true
+  isLeapYear(2015);      // false
+  isLeapYear(2100);      // false
+  isLeapYear(2400);      // true
+  isLeapYear(240000);    // true
+  isLeapYear(240001);    // false
+  isLeapYear(2000);      // true
+  isLeapYear(1900);      // false
+  isLeapYear(1752);      // true
+  isLeapYear(1700);      // true
+  isLeapYear(1);         // false
+  isLeapYear(100);       // true
+  isLeapYear(400);       // true
 
 */
 
 /*
 D:
 
+  data needed:
+    numbers
 */
 
 /*
 A:
 
+  Create a function that takes a number as the parameter
+
+  check with if statement if year is before 1752
+    if before 1752 then if number modulo of 4 is equal to 0
+    then return true
+
+  if after 1752 
+    if number % 4 equal to 0 
+    if number % 100 NOT equal to 0
+      return true
+    else if number % 400 equal to 0
+      return true
+  return false
 */
 
 //C:
+
+function isLeapYear(number) {
+  if (number < 1752) {
+    if (number % 4 === 0) {
+      return true;
+    }
+    return false;
+  } else {
+    if (number % 4 === 0) {
+      if (number % 100 !== 0) {
+        return true;
+      } else if (number % 400 === 0) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+console.log(isLeapYear(2016));      // true
+console.log(isLeapYear(2015));      // false
+console.log(isLeapYear(2100));      // false
+console.log(isLeapYear(2400));      // true
+console.log(isLeapYear(240000));    // true
+console.log(isLeapYear(240001));    // false
+console.log(isLeapYear(2000));      // true
+console.log(isLeapYear(1900));      // false
+console.log(isLeapYear(1752));      // true
+console.log(isLeapYear(1700));      // false
+console.log(isLeapYear(1));         // false
+console.log(isLeapYear(100));       // false
+console.log(isLeapYear(400));       // true
