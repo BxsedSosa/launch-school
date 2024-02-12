bac0ef8a7b5e51e8915b1299d38a9aa5952ea1cc
BxsedSosa <donovanxabad@gmail.com>
2024-02-12 07:36:45 -0800

    Fix Q6.js question

--- JS101-JS119-small/easy1/Q6.js
+++ JS101-JS119-small/easy1/Q6.js
@@ -1,17 +1,20 @@
 /*
 
-Tip Calculator
+Sum or Product of Consecutive Integers
 
-Create a simple tip calculator. The program should prompt for a bill amount and a tip rate. The program must compute the tip, and then log both the tip and the total amount of the bill to the console. You can ignore input validation and assume that the user will enter numbers.
+Write a program that asks the user to enter an integer greater than 0, then asks whether the user wants to determine the sum or the product of all numbers between 1 and the entered integer, inclusive.
 
-Example:
+Examples:
 
-What is the bill? 200
-What is the tip percentage? 15
+Please enter an integer greater than 0: 5
+Enter "s" to compute the sum, or "p" to compute the product. s
 
-The tip is $30.00
-The total is $230.00
+The sum of the integers between 1 and 5 is 15.
 
+Please enter an integer greater than 0: 6
+Enter "s" to compute the sum, or "p" to compute the product. p
+
+The product of the integers between 1 and 6 is 720.
 */
 
 /*
