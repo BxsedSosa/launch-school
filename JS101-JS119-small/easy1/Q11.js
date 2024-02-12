16b60ada76a7ba9c7fecaec1f3c8a175b794bfec
BxsedSosa <donovanxabad@gmail.com>
2024-02-12 12:45:22 -0800

    Completed Q11

--- JS101-JS119-small/easy1/Q11.js
+++ JS101-JS119-small/easy1/Q11.js
@@ -23,21 +23,65 @@ utf16Value(OMEGA + OMEGA + OMEGA);  // 2811
 /*
 P:
 
+  Question is asking to given a string return the UTF-16 value back
 */
 
 /*
 E:
+  Input:                            Output:
+  utf16Value('Four score');         // 984
+  utf16Value('Launch School');      // 1251
+  utf16Value('a');                  // 97
+  utf16Value('');                   // 0
 
+  const OMEGA = "\u03A9";             // UTF-16 character 'Ω' (omega)
+  utf16Value(OMEGA);                  // 937
+  utf16Value(OMEGA + OMEGA + OMEGA);  // 2811
 */
 
 /*
 D:
 
+  data needed:
+    string
 */
 
 /*
 A:
 
+  Create a function called utf16Value
+    1 parameter called string
+
+  create variable called stringLength
+    equal to string.length
+
+  create variable utfTotal
+    equal to 0
+
+  create a for loop iterating the string of the length of stringLength
+    takes the utfTotal equal to string.charCodeAt(index) + utfTotal
+  
+  return utfTotal
+
 */
 
 //C:
+
+function utf16Value(string) {
+  let stringLength = string.length;
+  let utfTotal = 0;
+
+  for (let i = 0; i < stringLength; i++) {
+    utfTotal += string.charCodeAt(i);
+  }
+  return utfTotal;
+}
+
+console.log(utf16Value('Four score'));         // 984
+console.log(utf16Value('Launch School'));      // 1251
+console.log(utf16Value('a'));                  // 97
+console.log(utf16Value(''));                   // 0
+
+const OMEGA = "\u03A9";             // UTF-16 character 'Ω' (omega)
+console.log(utf16Value(OMEGA));                  // 937
+console.log(utf16Value(OMEGA + OMEGA + OMEGA));  // 2811
