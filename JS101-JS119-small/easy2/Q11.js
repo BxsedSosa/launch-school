43c07aabe64e4932a79dd3ec7fe2b2466f0c3e25
BxsedSosa <donovanxabad@gmail.com>
2024-02-13 06:41:32 -0800

    Added each question into each file

---
+++ JS101-JS119-small/easy2/Q11.js
@@ -0,0 +1,16 @@
+/*
+
+
+Get Middle Character
+
+Write a function that takes a non-empty string argument and returns the middle character(s) of the string. If the string has an odd length, you should return exactly one character. If the string has an even length, you should return exactly two characters.
+
+Examples:
+
+centerOf('I Love JavaScript'); // "a"
+centerOf('Launch School');     // " "
+centerOf('Launch');            // "un"
+centerOf('Launchschool');      // "hs"
+centerOf('x');                 // "x"
+
+*/
---
+++ JS101-JS119-small/easy2/Q9.js
@@ -0,0 +1,12 @@
+/*
+
+
+How Old is Teddy?
+
+Build a program that randomly generates Teddy's age, and logs it to the console. Have the age be a random number between 20 and 120 (inclusive).
+
+Example Output:
+
+Teddy is 69 years old!
+
+*/
