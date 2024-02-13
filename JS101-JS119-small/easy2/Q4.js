f104394d9d37a7801c5d8604457b66929a5406d6
BxsedSosa <donovanxabad@gmail.com>
2024-02-13 08:48:07 -0800

    Completed Q4.js

--- JS101-JS119-small/easy2/Q4.js
+++ JS101-JS119-small/easy2/Q4.js
@@ -15,21 +15,58 @@ console.log(square(-8) === 64); // logs true
 /*
 P:
 
+  The question is asking to use the previous questions function of multiply to create a square root function that will take 1 parameter of a number and return the square root value of that number
 */
 
 /*
 E:
 
+  input:
+    5
+    -8
+
+  Ouput:
+    25
+    64
 */
 
 /*
 D:
 
+  data needed:
+    number
 */
 
 /*
 A:
 
+  Create function from previous question of multiply
+    which will take 2 parameters
+      Parameter 1 = number1
+      parameter 2 = number2
+
+    return number1 * number2
+
+  Create function called square
+    which will take 1 parameter
+      parameter = num
+
+    call multiply function
+      giving 2 arguments of num for both parameters
+
+    return then a comparison operation
+      num * num
 */
 
 //C:
+
+function square(num) {
+  return multiply(num, num);
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+console.log(square(5) === 25);
+console.log(square(-8) === 64);
