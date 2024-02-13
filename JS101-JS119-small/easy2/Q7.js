e33adae8f3fac63169e9f05ea6b889c76baf0636
BxsedSosa <donovanxabad@gmail.com>
2024-02-13 12:41:22 -0800

    Completed Q7.js

--- JS101-JS119-small/easy2/Q7.js
+++ JS101-JS119-small/easy2/Q7.js
@@ -19,21 +19,55 @@ console.log(xor(true, true) === false);   // true
 /*
 P:
 
+  Create a function called 'xor' that takes 2 parameters and we are checking that only 1 of the given arguments are considered true. if so return true. else if both arguments are true then return false. We are only looking for 1 of the arguments to be true NOT both
 */
 
 /*
 E:
 
+  Input:
+    5, 0
+    false, true
+    1, 1
+    true, true
+
+  Output:
+    true
+    true
+    true
+    true
 */
 
 /*
 D:
 
+  data needed:
+    2 arguments that are truthy values
 */
 
 /*
 A:
 
+  create function called xor
+    2 parameters
+      parameter 1 is value1
+      parameter 2 is value2
+
+    if value1 and value2
+      are equal to one another
+      return false
+
+    else 
+      return true
 */
 
 //C:
+
+function xor(value1, value2) {
+  return value1 === value2 ? false : true
+}
+
+console.log(xor(5, 0) === true);          // true
+console.log(xor(false, true) === true);   // true
+console.log(xor(1, 1) === false);         // true
+console.log(xor(true, true) === false);   // true
