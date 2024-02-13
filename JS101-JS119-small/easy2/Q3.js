117a7d4f015330d87846f82df3de150c3295d1b7
BxsedSosa <donovanxabad@gmail.com>
2024-02-13 08:27:17 -0800

    Completed Q3.js

--- JS101-JS119-small/easy2/Q3.js
+++ JS101-JS119-small/easy2/Q3.js
@@ -13,21 +13,45 @@ console.log(multiply(5, 3) === 15); // logs true
 /*
 P:
 
+  Question is asking to create a function called multiply
+    function takes 2 parameters
+
+  and multiply the 2 arugments given to one another and if it equals to 15 i will return true or false
 */
 
 /*
 E:
 
+  Input:
+    5, 3
+
+  Ouput:
+    true
 */
 
 /*
 D:
 
+  Data needed:
+    numbers
 */
 
 /*
 A:
 
+  Create a function called multiply that takes 2 parameters
+    parameter 1 = number1
+    parameter 2 = number2
+
+  return number1 multiply it to number2 
+
+  then do a comparison operiation to the number of 15
 */
 
 //C:
+
+function multiply(number1, number2) {
+  return number1 * number2;
+};
+
+console.log(multiply(5, 3) === number1 * number2);
