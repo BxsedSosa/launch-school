542add4d6332dcb3ee237c98bb46130774db3726
BxsedSosa <donovanxabad@gmail.com>
2024-02-13 08:03:58 -0800

    Completed Q2.js

--- JS101-JS119-small/easy2/Q2.js
+++ JS101-JS119-small/easy2/Q2.js
@@ -18,21 +18,57 @@ HELLO BOB. WHY ARE WE SCREAMING?
 /*
 P:
 
+  Question is asking to create a program will ask for your name and it will outupt hello <name>
 */
 
 /*
 E:
 
+  Input:
+    name from user
+
+  Output:
+    Hello <name>
+    HELLO <name with '!' uppercase all>. WHY ARE WE SCREAMING
 */
 
 /*
 D:
 
+  data needed: 
+    strings
 */
 
 /*
 A:
+  Create const called readline
+    require ('readline-sync')
 
+  Create function called helloName
+  
+  Ask user for name saved to variable userName
+
+  if the userName includes '!'
+    console log
+      HELLO <userName>. WHY ARE WE SCREAMING?
+
+  else
+    console log
+      Hello <userName>.
 */
 
 //C:
+
+const readline = require('readline-sync');
+
+function helloName() {
+  let userName = readline.question('What is your name?: \n');
+
+  if (userName.includes('!')) {
+    console.log(`HELLO ${userName.toUpperCase()}. WHY ARE WE SCREAMING?`);
+  } else {
+    console.log(`Hello ${userName}.`);
+  }
+}
+
+helloName();
