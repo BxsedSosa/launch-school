99dd1ba335c1ba061e25688a46ca3c5f68b50655
BxsedSosa <donovanxabad@gmail.com>
2024-02-13 14:15:52 -0800

    Completed Q12

--- JS101-JS119-small/easy2/Q12.js
+++ JS101-JS119-small/easy2/Q12.js
@@ -16,21 +16,49 @@ negative(0);     // -0
 /*
 P:
 
+  Create a function that takes 1 parameter of a number and make that number output as negative. If the number is already negative then just return it
 */
 
 /*
 E:
 
+  Input:  
+    5
+    -3
+    0
+
+  Output:
+    -5
+    -3
+    -0
 */
 
 /*
 D:
 
+  data needed:
+    numbers 
 */
 
 /*
 A:
 
+  Create a function called alwaysNegative takes 1 parameter
+    parameter is called number
+
+    if number is less than 0
+      return the number
+
+    else
+      return number times negative 1
 */
 
 //C:
+
+function alwaysNegative(number) {
+  return number < 0 ? number : number * -1;
+}
+
+console.log(alwaysNegative(5));
+console.log(alwaysNegative(-3));
+console.log(alwaysNegative(0));
