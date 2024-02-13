11c0779bac03c00c1825438f2eafe18d414ff1ad
BxsedSosa <donovanxabad@gmail.com>
2024-02-13 07:03:18 -0800

    Completed Q1.js

--- JS101-JS119-small/easy2/Q1.js
+++ JS101-JS119-small/easy2/Q1.js
@@ -17,21 +17,52 @@ console.log(
 /*
 P:
 
+  Question is asking to create a function that takes 2 parameters.
+  first parameter is a name broken up in a array and second parameter is a object with 2 keys 
+
+  it wants us to take the 2 parameters and print out a sentence using all the elements and keys given 
 */
 
 /*
 E:
 
+  Input:
+    ["John", "Q", "Doe"], { title: "Master", occupation: "Plumber" }
+
+  Output:
+    logs Hello, John Q Doe! Nice to have a Master Plumber around.
 */
 
 /*
 D:
 
+  Data needed:
+    Array
+    Object
+    Strings
 */
 
 /*
 A:
 
+  Create function called greetings
+    has 2 parameters:
+      Parameter 1 = array
+      Parameter 2 = object
+
+  Create a variable called joinedArray
+    using the .join method with a space to seperate the name
+
+  console log
+    hello, <name>! Nice to have <title> <occupatoin> around.
 */
 
 //C:
+
+function greetings(name, jobTitle) {
+  let joinedName = name.join(' ');
+
+  console.log(`Hello, ${joinedName}! Nice to have a ${jobTitle.title} ${jobTitle.occupation} around.`);
+}
+
+greetings(['John', 'Q', 'Doe'], {title: 'Master', occupation: 'Plumber'});
