3ea5256c6f63a43dd9cc266e5e9c19528f07d53a
BxsedSosa <donovanxabad@gmail.com>
2024-02-13 09:31:38 -0800

    Completed Q6.js

--- JS101-JS119-small/easy2/Q6.js
+++ JS101-JS119-small/easy2/Q6.js
@@ -19,21 +19,49 @@ console.log(penultimate("Launch School is great!") === "is"); // logs true
 /*
 P:
 
+  given a string, return the second to last word in the string
 */
 
 /*
 E:
 
+  input:
+    "last word"
+    "Launch School is great!"
+
+  output:
+    true
+    true
 */
 
 /*
 D:
 
+  Data needed:
+    string
 */
 
 /*
 A:
 
+  create function called penultimate
+    which will take 1 parameter
+      called string
+  
+    create a variable called stringArray
+      using the split method to break the string apart to an array
+
+    and then it will return stringArray variable of index -2
+
+  then compare it to the second to last word manually
 */
 
 //C:
+
+function penultimate(string) {
+  let stringArray = string.split(' ');
+  return stringArray[stringArray.length - 2];
+}
+
+console.log(penultimate("last word") === "last");
+console.log(penultimate("Launch School is great!") === "is");
