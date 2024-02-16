a196eeb671531f643dd1bac4e35e9cb7f460cd5f
BxsedSosa <donovanxabad@gmail.com>
2024-02-16 07:30:14 -0800

    Added in questions

---
+++ JS101-JS119-small/easy3/Q2.js
@@ -0,0 +1,28 @@
+/*
+
+Bannerizer
+
+Write a function that will take a short line of text, and write it to the console log within a box.
+
+Examples:
+
+logInBox('To boldly go where no one has gone before.');
+
+will log on the console:
+
++--------------------------------------------+
+|                                            |
+| To boldly go where no one has gone before. |
+|                                            |
++--------------------------------------------+
+
+logInBox('');
++--+
+|  |
+|  |
+|  |
++--+
+
+You may assume that the output will always fit in your browser window.
+
+*/
