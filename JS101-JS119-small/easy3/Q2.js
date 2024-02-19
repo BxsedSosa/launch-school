b11c8c9f562d5dfcf460d355e5a1aef6e16e9578
BxsedSosa <donovanxabad@gmail.com>
2024-02-19 13:06:03 -0800

    Completed Q2.js

--- JS101-JS119-small/easy3/Q2.js
+++ JS101-JS119-small/easy3/Q2.js
@@ -30,21 +30,59 @@ You may assume that the output will always fit in your browser window.
 /*
 P:
 
+  Need to create a function in which will take a string and insert the string within a bordered box and print out 
 */
 
 /*
 E:
 
+  Input:
+  'To boldly go where no one has gone before.'
+
+  Output:
+    +--------------------------------------------+
+    |                                            |
+    | To boldly go where no one has gone before. |
+    |                                            |
+    +--------------------------------------------+
 */
 
 /*
 D:
 
+  data needed:
+    string
 */
 
 /*
 A:
 
+  Create a function called logInBox which takes one parameter called str
+
+  create a variable called strLength equal to str.length
+
+  then console log with back ticks +--+ and in the middle of that we will use the ${} and insert '-' repeat method of the strLength
+
+  console log with back ticks |  | and in the middle of that we will use the ${}and inser ' ' repeat method of the strLength
+
+  console log with back ticks |  | and in the middle of that we will insert the parameter str
+
+  console log with back ticks |  | and in the middle of that we will use the ${}and inser ' ' repeat method of the strLength
+
+  then console log with back ticks +--+ and in the middle of that we will use the ${} and insert '-' repeat method of the strLength
 */
 
 //C:
+
+function logInBox(str) {
+  let strLength = str.length;
+
+  console.log(`+-${'-'.repeat(strLength)}-+`);
+  console.log(`| ${' '.repeat(strLength)} |`);
+  console.log(`| ${str} |`);
+  console.log(`| ${' '.repeat(strLength)} |`);
+  console.log(`+-${'-'.repeat(strLength)}-+`);
+  
+}
+
+logInBox('To boldly go where no one has gone before.');
