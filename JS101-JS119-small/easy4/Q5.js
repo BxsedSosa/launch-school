6dcd97f932e8b7314464fe1a0577aaa93c25c07a
BxsedSosa <donovanxabad@gmail.com>
2024-02-26 06:58:35 -0800

    Added in every question

---
+++ JS101-JS119-small/easy4/Q5.js
@@ -0,0 +1,36 @@
+/*
+
+Running Totals
+
+Write a function that takes an array of numbers and returns an array with the same number of elements, but with each element's value being the running total from the original array.
+
+Examples:
+
+runningTotal([2, 5, 13]);             // [2, 7, 20]
+runningTotal([14, 11, 7, 15, 20]);    // [14, 25, 32, 47, 67]
+runningTotal([3]);                    // [3]
+runningTotal([]);                     // []
+
+/*
+
+/*
+P:
+
+*/
+
+/*
+E:
+
+*/
+
+/*
+D:
+
+*/
+
+/*
+A:
+
+*/
+
+//C:
